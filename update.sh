63d5e7b0d87c87be9437388fd456264310999e96
tx0 <me@tx0.su>
2025-10-16 00:01:12 +0500

    [u] fix it

--- update.sh
+++ update.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
+set -exo pipefail
 
 REPO_LINK="https://$PAT_TOKEN@github.com/$REPO.git"
 
-date="$(LANG=ru_RU.UTF-8 date +'%A %d %B')"
+date="$(date +'%A %d %B')"
 
-git clone "$REPO_LINK" auto-commit
+git clone "$REPO_LINK" -b "$BRANCH_NAME" auto-commit
 
 pushd auto-commit
 
 cat << EOF > README.md 
 # Auto commit
-## Сегодня $date
+## Today is $date
 EOF
 
 git add README.md
 git commit -m "$date"
-git push "$REPO_LINK" -u origin "$BRANCH_NAME"
+git push -u origin "$BRANCH_NAME"
 
 popd
+
+rm auto-commit -rf
 
